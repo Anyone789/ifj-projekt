cdaa9c1338b07b96fd428091dc3c24dbf333acee
solis232 <solis123@seznam.cz>
2024-10-18 08:55:15 +0200

    Initial scanner interface version

---
+++ scanner.h
@@ -0,0 +1,39 @@
+// scanner.h
+// Interface for the scanner
+// Author(s): Václav Bergman
+// Last Edited: 18.10.2024
+
+
+// Enum of token types
+typedef enum
+{
+    KEYWORD, ERROR, ID, USER_FX_ID, BUILT_IN_FX_ID,
+    INT, STR, FLOAT, 
+    PLUS, MINUS, MUL, DIV,
+    EQ, NE, LT, LE, GT, GE, ASSIGN,
+    SEMI_COL, L_BRACE, R_BRACE, L_SQ_BRACKET, R_SQ_BRACKET,
+    L_BRACKET, R_BRACKET, QUESTION_MK, DOT
+} TOKEN_TYPE;
+
+// Union for storing token value
+typedef union
+{
+    int i;
+    float f;
+    char *str;
+    // ptr *kwrd - Pointer to a keyword in keyword table?
+} TOKEN_ATTRIBUTE;
+
+// The token
+typedef struct token_struct
+{
+    TOKEN_TYPE type;
+    TOKEN_ATTRIBUTE atribute;
+} TOKEN;
+
+
+// Temporary array of keywords. May be used in the future.
+const char* keywords[]
+    = { "const", "else", "fn", "if", "i32", "f64", 
+        "null", "pub", "return","u8", "[]u8", "var",
+        "void", "while" };
