f64d7f3000af0a22fc7806fd10af5221ccc78e3d
Majkyone <marian.sula2@gmail.com>
2024-10-26 11:04:44 +0200

    Pridanie kniznice pre zasobnik, este neodskusane

---
+++ stack.h
@@ -0,0 +1,28 @@
+// inicializacia zasobnika
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// struktura prvku v zasobniku, zatial bude value int, predpokladam, ze to bude asi nejaka struktura
+
+typedef struct stackItemStructure
+{
+    int *value;
+    stackItemStructure *next; 
+}TStackItem;
+
+// struktura zasobinka
+
+typedef struct stackStructure
+{
+   TStackItem *stackTop;
+}TStack;
+
+// deklaracia funkcii
+void stackInit(TStack *stack);
+void stackPush(TStack *stack, int *value);
+void stackPop(TStack *stack);
+int stackTop(TStack *stack);
+bool stackIsEmpty(TStack *stack);
+void stackDispose(TStack *stack);
