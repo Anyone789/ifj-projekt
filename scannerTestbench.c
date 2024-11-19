039b57c9703b1e0a296c55bdaeb91ac3cbf1ceca
solis232 <xbergmv00@stud.fit.vutbr.cz>
2024-11-19 10:26:33 +0100

    Added token type string representations

--- scannerTestbench.c
+++ scannerTestbench.c
@@ -1,12 +1,22 @@
 // Testbench for scanner.c
 // Author(s): Václav Bergman
-// Last Edit: 1.11.2024
+// Last Edit: 19.11.2024
 
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include "scanner.h"
 
+// String equivalents for token types
+const char *TOKEN_TYPE_STRING[] = {
+    "T_KEYWORD", "T_ERROR", "T_ID", "T_FX_ID", "T_BUILT_IN_FX_ID",
+    "T_INT", "T_STR", "T_FLOAT", 
+    "T_PLUS", "T_MINUS", "T_MUL", "T_DIV",
+    "T_EQ", "T_NE", "T_LT", "T_LE", "T_GT", "T_GE", "T_ASSIGN",
+    "T_SEMICOL", "T_COLON", "T_L_BRACE", "T_R_BRACE", "T_L_SQ_BRACKET", "T_R_SQ_BRACKET",
+    "T_L_BRACKET", "T_R_BRACKET", "T_QUESTION_MK", "T_PIPE", "T_DOT", "T_COMMA", "T_COM",
+    "T_EOF", "T_UNDEFINED"
+};
 
 int main(int argc, char **argv)
 {
