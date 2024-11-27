c3f9552d4a5a185429508e05f978a5915eb0e7d4
saminkosaurusrex <Jablonov1@>
2024-11-27 16:22:25 +0100

    built in fce

--- generator.c
+++ generator.c
@@ -44,6 +44,317 @@ void generateFunctionReturn(DSTRING *functionID)
     }
 }
 
-void generateFunctionCall(bstSymtable *symtableGlobal, bstSymtable *symtableLocal)
+// built in functions
+void generateReadStr()
 {
+    printf("LABEL readStr\n");
+    printf("CREATEFRAME\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@str\n");
+    printf("READ LF@str string\n");
+    printf("PUSHS LF@str\n");
+    printf("RETURN\n");
+}
+
+void generateReadInt()
+{
+    printf("LABEL readInt\n");
+    printf("CREATEFRAME\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@int\n");
+    printf("READ LF@int int\n");
+    printf("PUSHS LF@int\n");
+    printf("RETURN\n");
+}
+
+void generateReadFloat()
+{
+    printf("LABEL readFloat\n");
+    printf("CREATEFRAME\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@float\n");
+    printf("READ LF@float float\n");
+    printf("PUSHS LF@float\n");
+    printf("RETURN\n");
+}
+
+void generateWrite()
+{
+    printf("LABEL write\n");
+    printf("CREATEFRAME\n");
+    printf("PUSHFRAME\n");
+
+    printf("DEFVAR LF@term\n");
+    printf("DEFVAR LF@type\n");
+
+    printf("POPS LF@term\n");
+
+    printf("TYPE LF@type LF@term\n");
+
+    printf("JUMPIFEQ write_null LF@type string@nil\n");
+
+    printf("JUMPIFEQ write_int LF@type string@int\n");
+
+    printf("JUMPIFEQ write_float LF@type string@float\n");
+
+    printf("EXIT int@4\n");
+
+    printf("LABEL write_null\n");
+    printf("WRITE string@null\n");
+    printf("JUMP write_end\n");
+
+    printf("LABEL write_int\n");
+    printf("WRITE LF@term\n");
+    printf("JUMP write_end\n");
+
+    printf("LABEL write_float\n");
+    printf("WRITE LF@term\n");
+    printf("JUMP write_end\n");
+
+    printf("LABEL write_end\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+}
+void generateInt2Float()
+{
+    printf("LABEL i2f\n");
+    printf("CREATEFRAME\n");
+    printf("PUSHFRAME\n");
+
+    printf("DEFVAR LF@int\n");
+    printf("DEFVAR LF@float\n");
+
+    printf("POPS LF@int\n");
+
+    printf("INT2FLOAT LF@float LF@int\n");
+
+    printf("PUSHS LF@float\n");
+
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+}
+
+void generateFloat2Int()
+{
+    printf("LABEL f2i\n");
+    printf("CREATEFRAME\n");
+    printf("PUSHFRAME\n");
+
+    printf("DEFVAR LF@float\n");
+    printf("DEFVAR LF@int\n");
+
+    printf("POPS LF@float\n");
+
+    printf("FLOAT2INT LF@int LF@float\n");
+
+    printf("PUSHS LF@int\n");
+
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+}
+
+void generateString()
+{
+    printf("LABEL string\n");
+    printf("CREATEFRAME\n");
+    printf("PUSHFRAME\n");
+
+    printf("DEFVAR LF@term\n");
+    printf("DEFVAR LF@result\n");
+
+    printf("POPS LF@term\n");
+
+    printf("MOVE LF@result LF@term\n");
+
+    printf("PUSHS LF@result\n");
+
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+}
+
+void generateLenght()
+{
+    printf("LABEL length\n");
+    printf("CREATEFRAME\n");
+    printf("PUSHFRAME\n");
+
+    printf("DEFVAR LF@str\n");
+    printf("DEFVAR LF@length\n");
+
+    printf("POPS LF@str\n");
+
+    printf("STRLEN LF@length LF@str\n");
+
+    printf("PUSHS LF@length\n");
+
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+}
+
+void generateConcat()
+{
+    printf("LABEL concat\n");
+    printf("CREATEFRAME\n");
+    printf("PUSHFRAME\n");
+
+    printf("DEFVAR LF@str1\n");
+    printf("DEFVAR LF@str2\n");
+    printf("DEFVAR LF@result\n");
+
+    printf("POPS LF@str2\n");
+    printf("POPS LF@str1\n");
+
+    printf("CONCAT LF@result LF@str1 LF@str2\n");
+
+    printf("PUSHS LF@result\n");
+
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+}
+
+// ten global frame asi nieje najlpsi tuna netusim robil to cetak
+void generateSubstring()
+{
+    printf("LABEL substring\n");
+    printf("CREATEFRAME\n");
+    printf("PUSHFRAME\n");
+
+    // Definice proměnných
+    printf("DEFVAR LF@str\n");       // Vstupní řez
+    printf("DEFVAR LF@i\n");         // Počáteční index
+    printf("DEFVAR LF@j\n");         // Konečný index
+    printf("DEFVAR LF@result\n");    // Podřez
+    printf("DEFVAR LF@temp_char\n"); // Dočasný znak
+    printf("DEFVAR LF@length\n");    // Délka vstupního řezu
+
+    // Získání vstupů ze zásobníku
+    printf("POPS LF@j\n");
+    printf("POPS LF@i\n");
+    printf("POPS LF@str\n");
+
+    // Kontrola indexů
+    printf("STRLEN LF@length LF@str\n");
+    printf("LT GF@cond1 LF@i int@0\n");
+    printf("JUMPIFEQ substring_null GF@cond1 bool@true\n");
+    printf("LT GF@cond2 LF@j int@0\n");
+    printf("JUMPIFEQ substring_null GF@cond2 bool@true\n");
+    printf("GT GF@cond3 LF@i LF@j\n");
+    printf("JUMPIFEQ substring_null GF@cond3 bool@true\n");
+    printf("GE GF@cond4 LF@i LF@length\n");
+    printf("JUMPIFEQ substring_null GF@cond4 bool@true\n");
+    printf("GT GF@cond5 LF@j LF@length\n");
+    printf("JUMPIFEQ substring_null GF@cond5 bool@true\n");
+
+    // Vytvoření podřetězce
+    printf("MOVE LF@result string@\n");
+    printf("LABEL substring_loop\n");
+    printf("LT GF@cond6 LF@i LF@j\n");
+    printf("JUMPIFEQ substring_end GF@cond6 bool@false\n");
+    printf("GETCHAR LF@temp_char LF@str LF@i\n");
+    printf("CONCAT LF@result LF@result LF@temp_char\n");
+    printf("ADD LF@i LF@i int@1\n");
+    printf("JUMP substring_loop\n");
+
+    // Ukončení
+    printf("LABEL substring_end\n");
+    printf("PUSHS LF@result\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+
+    // Vrácení null při neplatných indexech
+    printf("LABEL substring_null\n");
+    printf("PUSHS nil@nil\n");
+    printf("POPFRAME\n");
+    printf("RETURN\n");
+}
+
+void generateStrCmp()
+{
+    printf("LABEL strcmp\n");
+    printf("CREATEFRAME\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@result\n");
+    printf("DEFVAR LF@i\n");
+    printf("DEFVAR LF@char_s1\n");
+    printf("DEFVAR LF@char_s2\n");
+
+    // Inicializace indexu
+    printf("DEFVAR LF@i\n");
+    printf("MOVE LF@i int@0\n");
+
+    // Smyčka pro porovnání
+    printf("LABEL strcmp_loop\n");
+    printf("GETCHAR LF@char_s1 LF@s1 LF@i\n");
+    printf("GETCHAR LF@char_s2 LF@s2 LF@i\n");
+    printf("JUMPIFNEQ strcmp_end\n");
+
+    // Pokud oba znaky jsou stejné, pokračuj
+    printf("ADD LF@i LF@i int@1\n");
+    printf("JUMP strcmp_loop\n");
+
+    // Pokud znaky nejsou stejné
+    printf("LABEL strcmp_end\n");
+    printf("JUMPIFGT LF@char_s1 LF@char_s2 THEN_GREATER\n");
+    printf("JUMPIFLT LF@char_s1 LF@char_s2 THEN_LESS\n");
+
+    // Pokud jsou znaky stejné, pokračujeme až do konce
+    printf("DEFVAR LF@result\n");
+    printf("MOVE LF@result int@0\n");
+    printf("RETURN\n");
+
+    printf("LABEL THEN_GREATER\n");
+    printf("MOVE LF@result int@1\n");
+    printf("RETURN\n");
+
+    printf("LABEL THEN_LESS\n");
+    printf("MOVE LF@result int@-1\n");
+    printf("RETURN\n");
+}
+
+void generateOrd()
+{
+    printf("LABEL ord\n");
+    printf("CREATEFRAME\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@char\n");
+    printf("DEFVAR LF@result\n");
+
+    // Získání znaku podle indexu
+    printf("GETCHAR LF@char LF@s LF@i\n");
+
+    // Kontrola platnosti indexu
+    printf("JUMPIFNULL ord_invalid\n");
+
+    // Vrátíme ASCII hodnotu znaku
+    printf("STRLEN LF@len LF@s\n");
+    printf("JUMPIFLT ord_invalid LF@i LF@len\n");
+
+    printf("ORD LF@result LF@char\n");
+    printf("RETURN\n");
+
+    printf("LABEL ord_invalid\n");
+    printf("MOVE LF@result int@0\n");
+    printf("RETURN\n");
+}
+
+void generateChr()
+{
+    printf("LABEL chr\n");
+    printf("CREATEFRAME\n");
+    printf("PUSHFRAME\n");
+    printf("DEFVAR LF@char\n");
+    printf("DEFVAR LF@result\n");
+
+    // Pokud je hodnota mimo platný rozsah (0-255), vrátíme null
+    printf("JUMPIFLT chr_invalid LF@i int@0\n");
+    printf("JUMPIFGT chr_invalid LF@i int@255\n");
+
+    // Vytvoření jednoznakového řetězce
+    printf("CHAR LF@char LF@i\n");
+    printf("MOVE LF@result LF@char\n");
+    printf("RETURN\n");
+
+    printf("LABEL chr_invalid\n");
+    printf("MOVE LF@result nil@nil\n");
+    printf("RETURN\n");
 }
--- generator.h
+++ generator.h
@@ -17,4 +17,157 @@ void generateFunctionHead(bstSymtable *symtable);
 void generateFunctionReturn(DSTRING *functionID);
 void generateFunctionEnd(bstSymtable *symtable);
 void generateFunctionCall(bstSymtable *symtableGlobal, bstSymtable *symtableLocal);
+// built in functions
+
+/**
+ * @brief Generates code for reading an integer from the standard input.
+ *
+ * This function generates code that reads an integer from the standard input.
+ * The input value is stored in a temporary variable.
+ *
+ * @return void
+ *
+ * @note This function does not return any value. It only generates code.
+ */
+void generateReadInt();
+
+/**
+ * @brief Generates code for reading a string from the standard input.
+ *
+ * This function generates code that reads a string from the standard input.
+ * The input string is stored in a temporary variable.
+ *
+ * @return void
+ *
+ * @note This function does not return any value. It only generates code.
+ */
+void generateReadStr();
+
+/**
+ * @brief Generates code for reading a floating-point number from the standard input.
+ *
+ * This function generates code that reads a floating-point number from the standard input.
+ * The input value is stored in a temporary variable.
+ *
+ * @return void
+ *
+ * @note This function does not return any value. It only generates code.
+ */
+void generateReadFloat();
+
+/**
+ * @brief Generates code for writing the value of a variable to the standard output.
+ *
+ * This function generates code that writes the value of a variable (could be of type string, integer, or float)
+ * to the standard output.
+ *
+ * @return void
+ *
+ * @note This function does not return any value. It only generates code.
+ */
+void generateWrite();
+
+/**
+ * @brief Generates code for converting an integer to a floating-point number.
+ *
+ * This function generates code to convert an integer into its floating-point representation.
+ *
+ * @return void
+ *
+ * @note This function does not return any value. It only generates code.
+ */
+void generateInt2Float();
+
+/**
+ * @brief Generates code for converting a floating-point number to an integer.
+ *
+ * This function generates code to convert a floating-point number to its integer representation,
+ * truncating the decimal part.
+ *
+ * @return void
+ *
+ * @note This function does not return any value. It only generates code.
+ */
+void generateFloat2Int();
+
+/**
+ * @brief Generates code for converting a string literal to a dynamic string representation.
+ *
+ * This function generates code that converts a string literal (or constant) to a dynamic string representation.
+ *
+ * @return void
+ *
+ * @note This function does not return any value. It only generates code.
+ */
+void generateString();
+
+/**
+ * @brief Generates code for getting the length of a string.
+ *
+ * This function generates code to calculate the length of a string and stores it in a variable.
+ *
+ * @return void
+ *
+ * @note This function does not return any value. It only generates code.
+ */
+void generateLenght();
+
+/**
+ * @brief Generates code for concatenating two strings.
+ *
+ * This function generates code that concatenates two strings into a single string.
+ *
+ * @return void
+ *
+ * @note This function does not return any value. It only generates code.
+ */
+void generateConcat();
+
+/**
+ * @brief Generates code for extracting a substring from a given string.
+ *
+ * This function generates code that extracts a substring from a given string using specified start and end indices.
+ *
+ * @return void
+ *
+ * @note This function does not return any value. It only generates code.
+ */
+void generateSubstring();
+
+/**
+ * @brief Generates code for comparing two strings lexicographically.
+ *
+ * This function generates code that compares two strings lexicographically and returns:
+ * -1 if the first string is less than the second,
+ * 0 if they are equal,
+ * 1 if the first string is greater than the second.
+ *
+ * @return void
+ *
+ * @note This function does not return any value. It only generates code.
+ */
+void generateStrCmp();
+
+/**
+ * @brief Generates code for getting the ordinal value (ASCII code) of a character in a string.
+ *
+ * This function generates code to obtain the ordinal value (ASCII code) of a character at a given index in a string.
+ *
+ * @return void
+ *
+ * @note This function does not return any value. It only generates code.
+ */
+void generateOrd();
+
+/**
+ * @brief Generates code for converting an ASCII value to a character string.
+ *
+ * This function generates code that converts an integer ASCII value to a single-character string.
+ *
+ * @return void
+ *
+ * @note This function does not return any value. It only generates code.
+ */
+void generateChr();
+
 #endif
